0a808cb4c6355fcea3cc98e5268fc1cfb11d0480
raghvendrayadav <raghvendrayadav503@gmail.com>
2026-09-17 13:09:11 +0700

    format terraform files

--- ec2.tf
+++ ec2.tf
@@ -25,35 +25,35 @@ resource "aws_security_group" "practice" {
   # HTTP
   ingress {
     from_port   = 80
-    to_port       = 80
-    protocol      = "tcp"
-    cidr_blocks   = ["0.0.0.0/0"]
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Outbound
   egress {
     from_port   = 0
     to_port     = 0
-    protocol     = "-1"
-    cidr_blocks  = ["0.0.0.0/0"]
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 # EC2
 resource "aws_instance" "aws_new" {
-    for_each = tomap({
-       instance_1 = "t2.micro"
-       instance_2 = "t2.medium"
-})#meta argument
-depends_on = [ aws_default_vpc.newvpc,aws_security_group.practice ]
-     key_name               = aws_key_pair.key.key_name
+  for_each = tomap({
+    instance_1 = "t2.micro"
+    instance_2 = "t2.medium"
+  }) #meta argument
+  depends_on             = [aws_default_vpc.newvpc, aws_security_group.practice]
+  key_name               = aws_key_pair.key.key_name
   vpc_security_group_ids = [aws_security_group.practice.id]
 
   instance_type = each.value
   ami           = "ami-07574623ba26e48f8"
 
   root_block_device {
-    volume_size = var.env == "production"  ? 20 : var.storage_default
+    volume_size = var.env == "production" ? 20 : var.storage_default
     volume_type = "gp3"
   }
 
--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "public_ip" {
-    value = [
-        for instance in aws_instance.aws_new : instance.public_ip
-    ]
-  
+  value = [
+    for instance in aws_instance.aws_new : instance.public_ip
+  ]
+
 }
